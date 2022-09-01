9c52a54e25cf38111d3ef6fe7ac89eac22cfa696
Enderson Barreto <enderson.frazao7@gmail.com>
2022-09-01 13:12:50 -0300

    feat: added a function of Galeria-trocar-img

--- js/script.js
+++ js/script.js
@@ -47,3 +47,23 @@ eventosPerguntas = (pergunta) => {
 }
 
 perguntas.forEach(eventosPerguntas)
+
+
+//Galeria de imagens-Bicicletas
+
+const galeria = document.querySelectorAll(".bicicleta-imagens img");
+const galeriaContainer = document.querySelector(".bicicleta-imagens");
+
+trocarImagem = (event) => {
+    const img = event.currentTarget;
+    const media = matchMedia('(min-width: 1000px)').matches;
+    if (media) {
+        galeriaContainer.prepend(img);
+    }
+}
+
+eventosGaleria = (img) => {
+    img.addEventListener('click', trocarImagem);
+}
+
+galeria.forEach(eventosGaleria);
